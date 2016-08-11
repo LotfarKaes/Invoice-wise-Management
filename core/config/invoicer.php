d4dc2ac15eeee99c072ec29a8baf31b4461dab42
lotfar <lutfurkayes@yahoo.com>
2016-08-11 13:51:33 +0200

    PHP file

---
+++ core/config/invoicer.php
@@ -0,0 +1,4 @@
+<?php return array('install' => true,
+                                          'version' => 2,
+                                          'install date' => '2016-07-01 02:40:08',
+                                        );
